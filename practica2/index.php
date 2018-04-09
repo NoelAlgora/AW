d149fa5c2aba8d529a382dea6bfbf7b9d78e3eff
jorgeboti9 <36920461+jorgeboti9@users.noreply.github.com>
2018-04-09 12:51:53 +0200

    Arreglo login
    Lo ha hecho carlos

--- practica2/index.php
+++ practica2/index.php
@@ -2,4 +2,3 @@
 <?php require('layout/header.php'); ?>
 <h4>Pagina principal</h4>
 <p> Esta es la pagina principal </p>
-<?php require('layout/footer.php'); ?>
--- practica2/pre_login.php
+++ practica2/pre_login.php
@@ -1,9 +1,18 @@
-<?php $pageTitle = "Login"; ?>
-<?php require('layout/header.php'); ?>
-<h4>Login</h4>
-<form class="login_form" action="log_login.php" method="post">
-  <p><label>Nombre de usuario</label><input type="text" name="username"/></p>
-  <p><label>Contraseña</label><input type="text" name="password"/></p>
-  <button type="submit">Log in</button>
-</form>
-<?php require('layout/footer.php'); ?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <title> Login  </title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+	<div class="login">
+		<div>
+			<h3> Login </h3>
+	    <form method="post">
+	    	<input class= "login_username" type="text" name="username" placeholder="Username" required="required" /><br>
+	        <input class= "login_password" type="password" name="password" placeholder="Password" required="required" /><br>
+	        <button class= "login_boton" type="submit">Log in</button>
+	    </form>
+	</div>	
+</html>
