3fa0c1bbc9c64742e025cd8cda55de82fbde171c
carlosmartin95 <martingutierrezcarlos@gmail.com>
2018-04-11 18:01:17 +0200

    bd

--- practica2/views/layout/nav.php
+++ practica2/views/layout/nav.php
@@ -3,8 +3,8 @@
    	<tr>
    	  <td><a class="nav_elements" href="<?= $helper->url('user','index');?>">INICIO</a></td>
    	  <td><a class="nav_elements" href="<?= $helper->url('user','alquiler');?>">ALQUILER</a></td>
-   	  <td><a class="nav_elements" href="<?= $helper->url('user','login');?>">RENTING</a></td>
-   	  <td><a class="nav_elements" href="<?= $helper->url('user','login');?>">CONTACTO</a></td>
+   	  <td><a class="nav_elements" href="">RENTING</a></td>
+   	  <td><a class="nav_elements" href="">CONTACTO</a></td>
    	</tr>
    </table>
  </nav>
