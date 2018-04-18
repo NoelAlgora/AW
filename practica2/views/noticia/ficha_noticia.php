b0aefd00b9610e108cd8b6b1c152ec0eadf41fc1
MIGUEL\miguel <mpomboza@ucm.es>
2018-04-18 13:44:41 +0200

    Añadido un link a cada noticia para que cuando pinches te envie a su ficha (te muestra la noticia entera)

--- practica2/views/noticia/ficha_noticia.php
+++ practica2/views/noticia/ficha_noticia.php
@@ -2,7 +2,7 @@
 <html lang="es">
 
 <head>
-  <?php $pageTitle = "Últimas noticias"; ?>
+  <?php $pageTitle = "Ficha noticia"; ?>
   <title><?php echo $pageTitle; ?></title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" >
@@ -11,23 +11,31 @@
 
 <body>
     <div id="container">
-        <p class="noticias"> Ultimas noticias </p>
+        <p class="noticias"> Noticia </p>
          <?php 
+         $ID = $_GET['id_noticia'];
+         //echo $var;
             $db = mysqli_connect('localhost','root','','caralcarro');
-            $listaNoticias = mysqli_query($db, "SELECT id_noticia, titulo, cuerpo_noticia FROM noticia");
-
+            $listaNoticia = mysqli_query($db, "SELECT titulo, autor, fecha, cuerpo_noticia FROM noticia WHERE id_noticia = $ID");
+            $noticia = mysqli_fetch_assoc($listaNoticia);
+            //$noticia = $db->query($listaNoticia);
             echo "<table>";   
-            while ($noticia = mysqli_fetch_assoc($listaNoticias)){
-              $id_n = $noticia['id_noticia'];  // guardamos el id de la noticia 
+            
               echo "<tr>";
               echo "<td class = 'listado_noticias'>";
               echo "<br><b>";
-              echo "<a href='../noticia/ficha_noticia.php?id_noticia=$id_n'> $noticia[titulo] </a>";
+              echo $noticia['titulo'];
               echo "</b></br>";
+              echo "<br>";
+              echo "Autor: ".$noticia['autor'];
+              echo "<br>";
+              echo "Fecha: ".$noticia['fecha'];
+              echo "</br>";
+              echo "<br>";
               echo $noticia['cuerpo_noticia'];
               echo "</td>";
               echo "</tr>";
-            }
+
             echo "</table>"; 
          ?>
     </div>
