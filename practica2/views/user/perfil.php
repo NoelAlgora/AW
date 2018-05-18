597afafe777de5f469ea5d222f8f99d5ec6caf27
fegonz <36365217+fegonz@users.noreply.github.com>
2018-05-18 13:22:14 +0200

    Add files via upload

--- practica2/views/user/perfil.php
+++ practica2/views/user/perfil.php
@@ -5,8 +5,10 @@
 	<title>
 		Perfil </title>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="stylesheet" type="text/css" src="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.3/animate.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/alquiler.css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
@@ -14,241 +16,250 @@
 	<div id="container">
 		<div id="contenido">
 
-			<h4>Informacion del usuario</h4>
-			<table class="table table-bordered">
-				<!--Table head-->
-				<thead>
-					<tr>
-						<th>nombre</th>
-						<th>apellidos</th>
-						<th>email</th>
-						<th>telefono</th>
-						<th>descripcion</th>
-					</tr>
-				</thead>
-				<!--Table head-->
+			<h1>Informacion del usuario</h1>
 
-				<!--Table body-->
-				<tbody>
-					<tr class="<?= ($libro['status'] == 0) ? 'danger' : 'success'?>">
-						<td>
-							<?= $datos['user']['username']?>
-						</td>
-						<td>
-							<?= $datos['user']['apellido']?>
-						</td>
-						<td>
-							<?= $datos['user']['email']?>
-						</td>
-						<td>
-							<?= $datos['user']['telefono']?>
-						</td>
-						<td>
-							<?= $datos['user']['descripcion']?>
-						</td>
-					</tr>
-				</tbody>
-			</table>
 
-			<h4>Coches Alquiler</h4>
-			<table class="table table-bordered">
-				<!--Table head-->
-				<thead>
-					<tr>
-						<th>#</th>
-						<th>marca</th>
-						<th>modelo</th>
-						<th>matricula</th>
-						<th>Fecha Inicio</th>
-						<th>Fecha Fin</th>
-						<th>Estado</th>
-						<th>Acciones</th>
-					</tr>
-				</thead>
-				<!--Table head-->
+		<div id="container">
+		<!-- <div id="contenido"> -->
+		<div class="container_cards">
+			<div class="cards">
+				
+				
+				<div class="card">
+					<a href="" >
+					<span class="card-header" style="<?php echo " background-image: url( " . $helper->getCarListImageCSS($value['list_img']) ."); "?>">
 
-				<!--Table body-->
-				<tbody>
+				<span class="card-title">
+					<h3><?php echo "Mis alquileres"; ?></h3>
+				</span>
+					</a>
+					</span>
+				<span class="card-summary">
+
+					
+
 					<?php
-					// var_dump($datos['rentCars']);
-					foreach ($datos['rentCars'] as $key => $car): ?>
-						<tr class="<?= ($libro['status'] == 0) ? 'danger' : 'success'?>">
-							<th scope="row">
-								<?= $key ?>
-							</th>
-							<td>
-								<?= $car['marca'] ?>
-							</td>
-							<td>
-								<?= $car['modelo'] ?>
-							</td>
-							<td>
-								<?= $car['matricula'] ?>
-							</td>
-							<td>
-								<?= $car['fecha_inicio'] ?>
-							</td>
-							<td>
-								<?= $car['fecha_fin'] ?>
-							</td>
-							<td class="text-center">
-								<form method="POST" action="<?= $helper->url('alquiler','devolver');?>">
-									<input type="hidden" name="alquiler_id" value="<?= $car['alquilerid'] ?>">
-									<button class="btn btn-success" type="submit">Devolver</button>
-								</form>
-								<td><a href="<?= $helper->url('alquiler', 'fichavehiculo', $car['id']) ?>"> Ver vehiculo</a></td>
-							</td>
-						</tr>
 
-						<?php endforeach; ?>
-				</tbody>
-				<!--Table body-->
+					$cont=count($datos['rentCars'] );
+
+					if($cont==0){
+						echo"No tienes ningún  en alquiler";
+					}else{
+						
+
+						if($cont==1){
+							echo "Tienes " .$cont." coche en alquiler";
+
+						}else{
+							echo "Tienes " .$cont." coches en alquiler";
+
+						}
+						echo '<br><a href="" ><input class="ctaFlat" type="button" value="Consultar"></a>';
+					}
+					?>
+				
+				
+				
+					
+				</div>
+				
+			</div>
+		</div>
+
+		<div id="container">
+		<!-- <div id="contenido"> -->
+		<div class="container_cards">
+			<div class="cards">
+				
+				
+				<div class="card">
+					<a href="" >
+					<span class="card-header" style="<?php echo " background-image: url( " . $helper->getCarListImageCSS($value['list_img']) ."); "?>">
+
+				<span class="card-title">
+					<h3><?php echo "Mis leasing"; ?></h3>
+				</span>
+					</a>
+					</span>
+				<span class="card-summary">
+
+					<?php
+
+					$cont=count($datos['leasingCars'] );
+
+					if($cont==0){
+						echo"No tienes ningún  con leasing";
+					}else{
+						
+						if($cont==1){
+							echo "Tienes " .$cont." coche  en leasing";
+
+						}else{
+							echo "Tienes " .$cont." coches en leasing";
+
+						}
+						echo '<br><a href="" ><input class="ctaFlat" type="button" value="Consultar"></a>';
+					}
+					?>
+
+					
+				
+				
+				
+					
+				</div>
+				
+			</div>
+		</div>
+
+		<div id="container">
+		<!-- <div id="contenido"> -->
+		<div class="container_cards">
+			<div class="cards">
+				
+				
+				<div class="card">
+					<a href="" >
+					<span class="card-header" style="<?php echo " background-image: url( " . $helper->getCarListImageCSS($value['list_img']) ."); "?>">
+
+				<span class="card-title">
+					<h3><?php echo "Coches propios en leasing"; ?></h3>
+				</span>
+					</a>
+					</span>
+				<span class="card-summary">
+
+					<?php
+
+					$cont=count($datos['ownedCarsToLeasing']);
+
+					if($cont==0){
+						echo"No tienes ningún coche propio en leasing";
+					}else{
+						
+
+						if($cont==1){
+							echo "Tienes " .$cont." coche propio  en leasing";
+
+						}else{
+							echo "Tienes " .$cont." coches propios  en leasing";
+
+						}
+						echo '<br><a href="" ><input class="ctaFlat" type="button" value="Consultar"></a>';
+					}
+					?>
+
+					
+				
+				
+				
+					
+				</div>
+				
+			</div>
+		</div>
+		<div id="container">
+		<!-- <div id="contenido"> -->
+		<div class="container_cards">
+			<div class="cards">
+				
+				
+				<div class="card">
+					<a href="<?php echo $helper->url('user','fichaMisAlquileresPropios', $value['id'])?>" >
+					<span class="card-header" style="">
+
+				<span class="card-title">
+					<h3><?php echo "Coches propios en alquiler"; ?></h3>
+				</span>
+					</a>
+					</span>
+				<span class="card-summary">
+					<div class="info_list_car">
+
+					<?php
+
+					$cont=count($datos['ownedCarsToAlquiler']);
+
+					if($cont==0){
+						echo"No tienes ningún coche propio en alquiler";
+
+						
 
 
-			</table>
+					}else{
+						if($cont==1){
+							echo "Tienes " .$cont." coche propio  en alquiler";
 
-			<h4>Coches Leasing</h4>
-			<table class="table table-bordered">
-				<!--Table head-->
-				<thead>
-					<tr>
-						<th>#</th>
-						<th>marca</th>
-						<th>modelo</th>
-						<th>matricula</th>
-						<th>Fecha Inicio</th>
-						<th>Fecha Fin</th>
-						<th>Estado</th>
-						<th>Acciones</th>
-					</tr>
-				</thead>
-				<!--Table head-->
+						}else{
+							echo "Tienes " .$cont." coches propios en alquiler";
 
-				<!--Table body-->
-				<tbody>
-					<?php	foreach ($datos['leasingCars'] as $key => $car): ?>
-						<tr class="<?= ($libro['status'] == 0) ? 'danger' : 'success'?>">
-							<th scope="row">
-								<?= $key ?>
-							</th>
-							<td>
-								<?= $car['marca'] ?>
-							</td>
-							<td>
-								<?= $car['modelo'] ?>
-							</td>
-							<td>
-								<?= $car['matricula'] ?>
-							</td>
-							<td>
-								<?= $car['fecha_inicio'] ?>
-							</td>
-							<td>
-								<?= $car['fecha_fin'] ?>
-							</td>
-							<td class="text-center">
-								<form method="POST" action="<?= $helper->url('leasing','devolver');?>">
-									<input type="hidden" name="leasing_id" value="<?= $car['leasingid'] ?>">
-									<button class="btn btn-success" type="submit">Devolver</button>
-								</form>
-								<td><a href="<?= $helper->url('leasing', 'fichavehiculo', $car['id']) ?>"> Ver vehiculo</a></td>
-							</td>
-						</tr>
-						<?php endforeach; ?>
-				</tbody>
-				<!--Table body-->
-			</table>
+						}
+						
 
-			<h4>Coches en propiedad destinados a alquiler</h4>
-			<table class="table table-bordered">
-				<!--Table head-->
-				<thead>
-					<tr>
-						<th>#</th>
-						<th>marca</th>
-						<th>modelo</th>
-						<th>matricula</th>
-						<th>motor</th>
-						<th>combustible</th>
-						<th>color</th>
-						<th>Acciones</th>
-					</tr>
-				</thead>
-				<!--Table head-->
-				<tbody>
-					<?php foreach($datos['ownedCarsToAlquiler'] as $key => $car): ?>
-					<tr class="<?= ($libro['status'] == 0) ? 'danger' : 'success'?>">
-						<th scope="row">
-							<?= $key ?>
-						</th>
-						<td>
-							<?= $car['marca'] ?>
-						</td>
-						<td>
-							<?= $car['modelo'] ?>
-						</td>
-						<td>
-							<?= $car['matricula'] ?>
-						</td>
-						<td>
-							<?= $car['motor'] ?>
-						</td>
-						<td>
-							<?= $car['combustible'] ?>
-						</td>
-						<td>
-							<?= $car['color'] ?>
-						</td>
-						<td><a href="<?= $helper->url('alquiler', 'fichavehiculo', $car['id']) ?>"> Ver vehiculo</a></td>
-					</tr>
-					<?php endforeach; ?>
-				</tbody>
-			</table>
-			<h4>Coches en propiedad destinados a leasing</h4>
-			<table class="table table-bordered">
-				<!--Table head-->
-				<thead>
-					<tr>
-						<th>#</th>
-						<th>marca</th>
-						<th>modelo</th>
-						<th>matricula</th>
-						<th>motor</th>
-						<th>combustible</th>
-						<th>color</th>
-						<th>Acciones</th>
-					</tr>
-				</thead>
-				<!--Table head-->
-				<tbody>
-					<?php foreach($datos['ownedCarsToLeasing'] as $key => $car): ?>
-					<tr class="<?= ($libro['status'] == 0) ? 'danger' : 'success'?>">
-						<th scope="row">
-							<?= $key ?>
-						</th>
-						<td>
-							<?= $car['marca'] ?>
-						</td>
-						<td>
-							<?= $car['modelo'] ?>
-						</td>
-						<td>
-							<?= $car['matricula'] ?>
-						</td>
-						<td>
-							<?= $car['motor'] ?>
-						</td>
-						<td>
-							<?= $car['combustible'] ?>
-						</td>
-						<td>
-							<?= $car['color'] ?>
-						</td>
-						<td><a href="<?= $helper->url('leasing', 'fichavehiculo', $car['id']) ?>"> Ver vehiculo</a></td>
-					</tr>
-					<?php endforeach; ?>
-				</tbody>
-			</table>
+						echo '<br><a href="" ><input class="ctaFlat" type="button" value="Consultar"></a>';
+					}
+					?>
+					
+				</div>
+				
+				
+					
+			</span>
+				</div>
+				
+				
+				
+					
+				</div>
+				
+			</div>
+		</div>
+		<div id="container">
+		<!-- <div id="contenido"> -->
+		<div class="container_cards">
+			<div class="cards">
+				<!-- <?php  var_dump($datos['result']); ?>	 -->
+				
+				<div class="card">
+					<a href="<?= $helper->url('user','fichaNoticia', $noticia['id']) ?>" >
+					<span class="card-header" style="">
+
+				<span class="card-title">
+					<h3><?php echo "Mis datos personales"; ?></h3>
+				</span>
+					</a>
+					</span>
+				<span class="card-summary">
+				<div class="info_list_car">
+					<img class="symbol_list_car" src="img/engine.png" alt="engine">
+					<p><?= $datos['user']['username']?></p>
+					<img class="symbol_list_car" src="img/fuel.png" alt="fuel">
+					<p><?= $datos['user']['apellido']?></p>
+				</div>
+				
+				<div class="info_list_car">
+					<img class="symbol_list_car" src="img/gear.png" alt="cambio">
+					<p><?= $datos['user']['email']?></p>
+					<img class="symbol_list_car" src="img/passenger.png" alt="cambio">
+					<p></p>
+				</div>
+				<div class="info_list_car">
+					<img class="symbol_list_car" src="img/price.png" alt="price">
+					<p><?= $datos['user']['telefono']?> </p>
+				</div>
+					<a href="" ><input class="ctaFlat" type="button" value="Modificar"></a>
+			</span>
+				</div>
+				
+			</div>
+		</div>
+		
+	</div>
+
+
+		
+	</div>
+			
 		</div>
 	</div>
 	<?php require('views/layout/footer.php'); ?>
